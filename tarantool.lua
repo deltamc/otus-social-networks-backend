29d131aff4c68ea603e4235f777da9d526dd9da5
deltamc <biomars@yandex.ru>
2022-08-17 21:56:59 +0300

    tarantool

--- tarantool.lua
+++ tarantool.lua
@@ -1,10 +1,10 @@
 box.cfg{
     listen = 3301,
-    pid_file = nil,
-    background = true,
-    log_level = 5,
-    memtx_memory = 512 * 1024 * 1024,
-    memtx_max_tuple_size = 8 * 1024 * 1024
+--     pid_file = nil,
+--     background = false,
+--     log_level = 5,
+--     memtx_memory = 512 * 1024 * 1024,
+--     memtx_max_tuple_size = 8 * 1024 * 1024
 }
 
 box.schema.user.create('user', {password='password', if_not_exists=true})
@@ -37,3 +37,8 @@ s:create_index(
 
 
 s:insert{10000000000, 'first_name', 'last_name', 25, 1, 'fdsafdasfds','Moscow'}
+
+-- function get_users(first_name, last_name)
+--     local tup = box.space.users.index.secondary:select({id}, {iterator='GE'})
+--     return
+-- end
