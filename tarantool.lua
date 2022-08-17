8b8446caa30de9a263118f83b251da15a33176b3
deltamc <biomars@yandex.ru>
2022-08-17 22:15:38 +0300

    tarantool

--- tarantool.lua
+++ tarantool.lua
@@ -38,7 +38,7 @@ s:create_index(
 
 s:insert{10000000000, 'first_name', 'last_name', 25, 1, 'fdsafdasfds','Moscow'}
 
--- function get_users(first_name, last_name)
---     local tup = box.space.users.index.secondary:select({id}, {iterator='GE'})
---     return
--- end
+function get_users(first_name, last_name)
+    local tup = box.space.users.index.secondary:select({first_name, last_name})
+    return
+end
