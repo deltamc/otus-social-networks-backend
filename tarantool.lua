d546e118f11a2c879514bf73ee4a3ba9a7d1d251
deltamc <biomars@yandex.ru>
2022-08-17 21:21:45 +0300

    tarantool

---
+++ tarantool.lua
@@ -0,0 +1,33 @@
+box.cfg{
+    listen = 3301
+    pid_file = nil,
+    background = true,
+    log_level = 5,
+    memtx_memory = 512 * 1024 * 1024,
+    memtx_max_tuple_size = 8 * 1024 * 1024
+}
+box.schema.user.create('user', {password='password', if_not_exists=true})
+box.schema.user.grant('user', 'read,write', 'universe')
+
+s = box.schema.space.create('users')
+s:format({
+         {name = 'id', type = 'unsigned'},
+         {name = 'first_name', type = 'string'},
+         {name = 'last_name', type = 'string'},
+         {name = 'age', type = 'unsigned'},
+         {name = 'sex', type = 'unsigned'},
+         {name = 'interests', type = 'string'},
+         {name = 'city', type = 'string'},
+         })
+s:create_index('primary', {
+         > type = 'tree',
+         > parts = {'id'}
+         > })
+news:create_index(
+        'secondary',
+        {
+            type = 'tree',
+            unique = false,
+            parts = {'first_name', 'last_name', 'rubric_id'}
+        }
+)
