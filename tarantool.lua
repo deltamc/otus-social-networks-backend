26056f534efcbc00a238de552570fe5c9e11d152
deltamc <biomars@yandex.ru>
2022-08-22 22:41:59 +0300

    ta

--- tarantool.lua
+++ tarantool.lua
@@ -1,8 +1,8 @@
 box.cfg{
     listen = 3301,
-     background = true,
-       log = '1.log',
-       pid_file = '1.pid'
+--      background = true,
+--        log = '1.log',
+--        pid_file = '1.pid'
 }
 
 -- box.schema.user.create('user', {password='password', if_not_exists=true})
@@ -36,6 +36,8 @@ box.cfg{
 --
 -- s:insert{10000000000, 'first_name', 'last_name', 25, 1, 'fdsafdasfds','Moscow'}
 
-function get_users(first_name, last_name)
-    return box.space.users.index.secondary:select({first_name, last_name})
-end
+-- function get_users(first_name, last_name)
+--     return box.space.users.index.secondary:select({first_name, last_name})
+-- end
+
+box.space.users.index.secondary:select({})
